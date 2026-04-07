d3f89418a17221c3a8c8853bd18af4ab918f1fa7
Mustea Dan <danmusteadan@gmail.com>
2026-04-07 22:25:36 +0300

    -Added custom generic Repository and UnitOfWork; -Set up connection string to local SQLServer DB

---
+++ DeviceManagement_API/Infrastructure/Extensions/DatabaseExtension.cs
@@ -0,0 +1,25 @@
+﻿using Application.Abstraction;
+using Infrastructure.Persistence.Repository;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure.Extensions;
+public static class DatabaseExtension
+{
+    public static void AddUnitOfWork(this IServiceCollection services)
+    {
+        services.AddScoped<SqlConnection>(sp =>
+        {
+            var config = sp.GetRequiredService<IConfiguration>();
+            var connString = config.GetConnectionString("SQLServer_Connection_String");
+
+            var connection = new SqlConnection(connString);
+            connection.Open(); 
+
+            return connection;
+        });
+
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+    }
+}
