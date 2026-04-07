a5d6b5506210766ddb47d789d5c6d7a0d9a0a3d1
Mustea Dan <danmusteadan@gmail.com>
2026-04-07 23:38:34 +0300

    -Implemented first test user controller

---
+++ DeviceManagement_API/Presentation/Controllers/UserController.cs
@@ -0,0 +1,46 @@
+﻿using Application.Abstraction;
+using Application.CommandsAndQueries.Users;
+using Application.DTOs;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+[Authorize]
+public class UserController : Controller
+{
+
+    private readonly IMediator _mediator;
+
+    public UserController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+
+    [AllowAnonymous]
+    [HttpPost]
+    [Route("sign-up")]
+    public async Task<IActionResult> CreateUserAsync([FromBody] UserRequestDTO userDto, CancellationToken cancellationToken)
+    {
+        if (userDto is null)
+        {
+            return BadRequest("User data is required.");
+        }
+
+        var result = await _mediator.Send(new CreateUserCommand(userDto), cancellationToken);
+
+        if (result.IsFailure)
+        {
+            return BadRequest(result.Error);
+        }
+
+        return Ok();
+
+    }
+
+    
+}
+
